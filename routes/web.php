5d17759ac36c1b3f252a3f1f41a2521504fa8036
sheilanvv <nvidiasheila@students.amikom.ac.id>
2026-04-10 10:12:07 +0700

    Update: Tugas 1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+Route::get('/tentang', function () {
+    return '<h1>Ini adalah Halaman Tentenag Aplikasi Event Hub</h1>';
+});
+
+Route::get('/kontak', function () {
+    return view('contact');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/katalog', function () {
+    return view('katalog');
+});
+
+Route::get('/bantuan', function () {
+    return view('bantuan');
 });
