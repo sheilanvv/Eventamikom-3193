1774d533a37bb6ab1962a602ba6e19dbd2427855
sheilanvv <nvidiasheila@students.amikom.ac.id>
2026-04-17 10:45:52 +0700

    latihan1

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
 
+Route::get('/',[HomeController::class, 'index']
+)->name('home');
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
+Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
 Route::get('/tentang', function () {
     return '<h1>Ini adalah Halaman Tentenag Aplikasi Event Hub</h1>';
 });
@@ -25,3 +30,24 @@ Route::get('/katalog', function () {
 Route::get('/bantuan', function () {
     return view('bantuan');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+Route::get('/event', function () {
+    return view('admin.events');
+})->name('admin.events');
+
+Route::get('/transactions', function () {
+    return view('admin.transactions');
+})->name('admin.transactions');
+
+// Route::group(['prefix' => 'admin','as' => 'admin.'], function () {
+
+// Route::get('/', [DashboardController::class,'index'])->name('dashboard');
+
+// Route::get('/events', [EventController::class,'indexAdmin'])->name('events.index');
+
+
+// });
